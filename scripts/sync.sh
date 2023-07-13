f6b64515bc1dfb4ca1a38800ae9029b43fbadcbc
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-07-13 18:44:47 +0700

    Change worker to 12 [skip ci]

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,7 +5,7 @@ cd ~
 
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mitsubit/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j12
 
 # Exit
 exit 0
