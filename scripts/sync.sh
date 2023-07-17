677bee7735795665cdaebfe4ae149a582860b2d2
mitsu00 <dev@ozip.my.id>
2023-07-17 14:08:35 +0700

    Change local manifest [skip ci]

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,8 +4,8 @@
 cd ~
 
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b los18-test .repo/local_manifests
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j10
+git clone https://github.com/mitsubit/local.git --depth 1 -b a11 .repo/local_manifests
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 # Exit
 exit 0
