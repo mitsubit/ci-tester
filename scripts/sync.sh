87355102f727fabda12f6a28b7dae5220a7319cb
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-01 14:15:20 +0700

    switch to AlphaDroid test

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,8 +3,8 @@
 # Change to the Home Directory
 cd ~
 
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsubit/local.git --depth 1 -b a11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsubit/local.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 # Exit
