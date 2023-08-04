dcc26736281e9b7e151132b16297a7a0addeea70
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-04 12:17:24 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,8 +3,8 @@
 # Change to the Home Directory
 cd ~
 
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsubit/local.git --depth 1 -b arrow-13.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest.git -b alpha-13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsubit/local.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 # Exit
