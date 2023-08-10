e961fd287facc03a6c58c01ad0a0b18a491d039d
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-10 13:15:09 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,8 +3,8 @@
 # Change to the Home Directory
 cd ~
 
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest.git -b alpha-13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsubit/local.git --depth=1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest.git -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 # Exit
