b561163f0d4156b70704deb7fe1526bbf40daf93
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-19 22:37:59 +0700

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,8 +2,8 @@
 
 cd ~
 echo "Uploading File"
-curl -F file=@out/target/product/merlinx/*.zip https://store1.gofile.io/uploadFile
-curl -T out/target/product/merlinx/*.zip temp.sh
+#curl -F file=@out/target/product/merlinx/*.zip https://store1.gofile.io/uploadFile
+curl bashupload.com -T out/target/product/merlinx/*.zip
 #curl -T out/target/product/merlinx/boot.img temp.sh
 
 # Exit
