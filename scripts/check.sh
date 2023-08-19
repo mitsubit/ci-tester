720f7dfa04956afe0883c9862df6b1446b104bb2
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-19 21:01:06 +0700

    Update check.sh

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,9 +2,14 @@
 
 echo "Check Free RAM:"
 free -h
-echo "\n"
+echo "-----------------"
+echo ""
 echo "Check Free Disk:"
 df -h
-echo "\n"
+echo "-----------------"
+echo ""
+echo "clang check: "
+ls /root/prebuilts/clang/host/linux-x86/clang-r450784d
+
 
 exit 0
