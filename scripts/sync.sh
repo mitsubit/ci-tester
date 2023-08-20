70c2d3b86d382b0d3d027509619d89a029c3d5b7
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-20 14:09:31 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,9 +3,9 @@
 # Change to the Home Directory
 cd ~
 
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r74
 git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b ci .repo/local_manifests
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j18
+repo sync -qc --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 echo "clang check: "
 ls /root/prebuilts/clang/host/linux-x86/
