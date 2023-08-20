2e8e5c309649ac71c77bac3b71c2afcd3e3b1fda
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-20 11:44:18 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,7 +3,7 @@
 # Change to the Home Directory
 cd ~
 
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest.git -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b ci .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j18
 
