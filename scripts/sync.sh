b68c725d07f11fda8124287f4362f7e8b659b586
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-08-23 16:33:55 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,7 +4,7 @@
 cd ~
 
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b alpha-13 .repo/local_manifests
+git clone https://github.com/mitsu00/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -qc --no-clone-bundle --optimized-fetch --prune --force-sync --fail-fast -j8
 
 echo "clang check: "
